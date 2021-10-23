f6c653c2dd7ef8258296110a911a229d73494325
nikhil051102 <nikhildeore05@gmail.com>
2021-10-23 15:54:25 +0530

    -

---
+++ Code With Harry/Dynamic Memory Allocation/Dynamic_Memory_Allocation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//If we run any function then a memory in Stack is allocated to it once it returns any value all the memory gets free.
+
+//Dynamic Memory Allocation is used to allocate Memory at RunTime. Dynamic Memory Allocation is the way in which the size of data structure can be changed during run time.
+
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
